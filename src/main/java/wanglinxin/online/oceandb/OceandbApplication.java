a95be509dbee36b92b03f305aeae5e337468ccaa
wanglinxin <to2000s@163.com>
2020-07-03 21:47:42 +0800

    create repository

---
+++ src/main/java/wanglinxin/online/oceandb/OceandbApplication.java
@@ -0,0 +1,13 @@
+package wanglinxin.online.oceandb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OceandbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OceandbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wanglinxin/online/oceandb/OceandbApplicationTests.java
@@ -0,0 +1,13 @@
+package wanglinxin.online.oceandb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OceandbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
